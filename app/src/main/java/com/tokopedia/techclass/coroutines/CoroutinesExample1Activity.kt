50ec3ffbdd9805ed30b6bc23520728e83bedf3bf
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-07-08 22:01:57 +0700

    coroutine async example

--- app/src/main/java/com/tokopedia/techclass/coroutines/CoroutinesExample1Activity.kt
+++ app/src/main/java/com/tokopedia/techclass/coroutines/CoroutinesExample1Activity.kt
@@ -2,24 +2,59 @@ package com.tokopedia.techclass.coroutines
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.tokopedia.techclass.R
 import kotlinx.android.synthetic.main.activity_coroutines_example_1.*
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class CoroutinesExample1Activity : AppCompatActivity() {
+class CoroutinesExample1Activity : AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutines_example_1)
+
         button.setOnClickListener {
-            GlobalScope.launch(Dispatchers.IO) { // launch a new coroutine in background and continue
-                delay(1000) // non-blocking delay for 1 second (default time unit is ms)
-                println("World") // print after delay
+            launch {
+                delay(1000)
+                val randomValue = (Math.random() * 100).toInt().toString()
+                withContext(Dispatchers.Main) {
+                    textView.text = randomValue
+                }
             }
-            println("Hello,") // main thread continues while coroutine is delayed
         }
 
+        button_2.setOnClickListener {
+            launch {
+                delay(1000)
+                val deferred = async(Dispatchers.IO) {
+                    (Math.random() * 100).toInt().toString()
+                }
+                val result = deferred.await()
+                withContext(Dispatchers.Main) {
+                    textView_2.text = result
+                }
+            }
+        }
+
+        button_3.setOnClickListener {
+            launch {
+                val deferred1 = async(Dispatchers.IO) {
+                    Thread.sleep(2000)
+                    "Jakarta"
+                }
+                val deferred2 = async(Dispatchers.IO) {
+                    Thread.sleep(2000)
+                    "Bandung"
+                }
+                val result = deferred1.await() + " " + deferred2.await()
+                withContext(Dispatchers.Main) {
+                    textView_3.text = result
+                }
+            }
+        }
     }
+
 
 }
